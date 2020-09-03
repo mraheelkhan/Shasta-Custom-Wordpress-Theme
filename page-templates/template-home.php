6342718cde6926432e03d1f7e6210a37491450c7
Raheel Khan <irahilkhan@gmail.com>
2020-09-03 19:37:51 +0500

    homepage

--- page-templates/template-home.php
+++ page-templates/template-home.php
@@ -12,21 +12,34 @@ defined( 'ABSPATH' ) || exit;
 
 get_header();
 ?>
-<div class="home-banner">
-    <div class="image">
-        
-    </div>
-</div>
+<div class="homepage">
 
-<div class="below-banner-section">
-    <div class="container">
-        <div class="below-banner-text">
-            <h2>we helped <span class="text-light-blue">tien</span></h2>
-            <h2>reach new heights 
-            </h2>
-            <div class="after-text-line"></div>
+    <div class="home-banner">
+        <div class="image">
+            
         </div>
-
+    </div>
+    
+    <div class="below-banner-section">
+        <div class="container">
+            <div class="below-banner-text">
+                <h2>we helped <span class="text-light-blue">tien</span></h2>
+                <h2 class="h2-class">reach new heights 
+                </h2>
+            </div>
+    
+        </div>
+    </div>
+    <div class="on-click-footer-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="arrow">
+                        <i class="fa fa-chevron-down fa-lg"></i>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
 
