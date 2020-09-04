64b99cda3ccf0cbeb7678ceb229abe6f23818b16
Raheel Khan <irahilkhan@gmail.com>
2020-09-05 02:04:23 +0500

    founder templates

--- page-templates/template-shasta-page.php
+++ page-templates/template-shasta-page.php
@@ -40,6 +40,7 @@ get_header();
         <div class="container-fluid">
             <div class="row">
                 <div class="col-md-3 m-0 p-0">
+                    <a href="<?= site_url() . '/bio-details';?>">
                     <div class="person-image person-image-1" style="background-image: url(<?php echo site_url() . $image_dir ?>person1.png);">
                         <p>
                         Avery <br>Cannon
@@ -49,105 +50,141 @@ get_header();
                             <div class="text">John Doe</div>
                         </div>
                     </div>
+                    </a>
                 </div>
                 
                 <div class="col-md-3 m-0 p-0">
+                    <a href="<?= site_url() . '/bio-details';?>">
                     <div class="person-image person-image-1" style="background-image: url(<?php echo site_url() . $image_dir ?>jason_pressman.png);">
                         <p>
                         Jason <br>Pressman
                         </p>
                         <div class="divider-person"></div>
                     </div>
+                    </a>
                 </div>
                 <div class="col-md-3 m-0 p-0">
+                    <a href="<?= site_url() . '/bio-details';?>">
                     <div class="person-image person-image-1" style="background-image: url(<?php echo site_url() . $image_dir ?>ravi_mohan.png);">
                         <p>
                         Ravi <br>Mohan
                         </p>
                         <div class="divider-person"></div>
                     </div>
+                    </a>
                 </div>
                 <div class="col-md-3 m-0 p-0">
+                    <a href="<?= site_url() . '/bio-details';?>">
                     <div class="person-image person-image-1" style="background-image: url(<?php echo site_url() . $image_dir ?>person1.png);">
                         <p>
                         Eiena <br>Ennouri
                         </p>
                         <div class="divider-person"></div>
                     </div>
+                    </a>
                 </div>
                 <div class="col-md-3 m-0 p-0">
+                    <a href="<?= site_url() . '/bio-details';?>">
                     <div class="person-image person-image-1" style="background-image: url(<?php echo site_url() . $image_dir ?>jacub.png);">
                         <p>
                         Jacob <br>Mullins
                         </p>
                         <div class="divider-person"></div>
                     </div>
+                    </a>
                 </div>
                 <div class="col-md-3 m-0 p-0">
+                    <a href="<?= site_url() . '/bio-details';?>">
                     <div class="person-image person-image-1" style="background-image: url(<?php echo site_url() . $image_dir ?>catlin.png);">
                         <p>
                         Caitlin <br>Swofford
                         </p>
                         <div class="divider-person"></div>
                     </div>
+                    </a>
                 </div>
                 <div class="col-md-3 m-0 p-0">
+                    <a href="<?= site_url() . '/bio-details';?>">
                     <div class="person-image person-image-1" style="background-image: url(<?php echo site_url() . $image_dir ?>isaac.png);">
                         <p>
                         Issac <br>Roth
                         </p>
                         <div class="divider-person"></div>
                     </div>
+                    </a>
                 </div>
                 <div class="col-md-3 m-0 p-0">
+                    <a href="<?= site_url() . '/bio-details';?>">
                     <div class="person-image person-image-1" style="background-image: url(<?php echo site_url() . $image_dir ?>nitin.png);">
                         <p>
                         Nitin <br>Chopra
                         </p>
                         <div class="divider-person"></div>
                     </div>
+                    </a>
                 </div>
                 <div class="col-md-3 m-0 p-0">
+                    <a href="<?= site_url() . '/bio-details';?>">
                     <div class="person-image person-image-1" style="background-image: url(<?php echo site_url() . $image_dir ?>ken_so.png);">
                         <p>
                         Ken <br>So
                         </p>
                         <div class="divider-person"></div>
                     </div>
+                    </a>
                 </div>
                 <div class="col-md-3 m-0 p-0">
+                    <a href="<?= site_url() . '/bio-details';?>">
                     <div class="person-image person-image-1" style="background-image: url(<?php echo site_url() . $image_dir ?>person1.png);">
                         <p>
                         Vy <br>Tran
                         </p>
                         <div class="divider-person"></div>
                     </div>
+                    </a>
                 </div>
                 <div class="col-md-3 m-0 p-0">
+                    <a href="<?= site_url() . '/bio-details';?>">
                     <div class="person-image person-image-1" style="background-image: url(<?php echo site_url() . $image_dir ?>alex.png);">
                         <p>
                         Alex <br>Priento
                         </p>
                         <div class="divider-person"></div>
                     </div>
+                    </a>
                 </div>
                 <div class="col-md-3 m-0 p-0">
+                    <a href="<?= site_url() . '/bio-details';?>">
                     <div class="person-image person-image-1" style="background-image: url(<?php echo site_url() . $image_dir ?>mike_trig.png);">
                         <p>
                         Mike <br>Trig
                         </p>
                         <div class="divider-person"></div>
                     </div>
+                    </a>
                 </div>
                 <div class="col-md-3 m-0 p-0">
+                    <a href="<?= site_url() . '/bio-details';?>">
                     <div class="person-image person-image-1" style="background-image: url(<?php echo site_url() . $image_dir ?>person1.png);">
                         <p>
                         Rose <br>Kelly
                         </p>
                         <div class="divider-person"></div>
                     </div>
+                    </a>
                 </div>
                 <div class="col-md-3 m-0 p-0">
+                    <a href="<?= site_url() . '/bio-details';?>">
+                    <div class="person-image person-image-1" style="background-image: url(<?php echo site_url() . $image_dir ?>person1.png);">
+                        <p>
+                        Avery <br>Cannon
+                        </p>
+                        <div class="divider-person"></div>
+                    </div>
+                    </a>
+                </div>
+                <div class="col-md-3 m-0 p-0">
+                    <a href="<?= site_url() . '/bio-details';?>">
                     <div class="person-image person-image-1" style="background-image: url(<?php echo site_url() . $image_dir ?>person1.png);">
                         <p>
                         Avery <br>Cannon
@@ -156,60 +193,64 @@ get_header();
                     </div>
                 </div>
                 <div class="col-md-3 m-0 p-0">
+                    <a href="<?= site_url() . '/bio-details';?>">
                     <div class="person-image person-image-1" style="background-image: url(<?php echo site_url() . $image_dir ?>person1.png);">
                         <p>
                         Avery <br>Cannon
                         </p>
                         <div class="divider-person"></div>
                     </div>
+                    </a>
                 </div>
                 <div class="col-md-3 m-0 p-0">
+                    <a href="<?= site_url() . '/bio-details';?>">
                     <div class="person-image person-image-1" style="background-image: url(<?php echo site_url() . $image_dir ?>person1.png);">
                         <p>
                         Avery <br>Cannon
                         </p>
                         <div class="divider-person"></div>
                     </div>
+                    </a>
                 </div>
                 <div class="col-md-3 m-0 p-0">
+                    <a href="<?= site_url() . '/bio-details';?>">
                     <div class="person-image person-image-1" style="background-image: url(<?php echo site_url() . $image_dir ?>person1.png);">
                         <p>
                         Avery <br>Cannon
                         </p>
                         <div class="divider-person"></div>
                     </div>
+                    </a>
                 </div>
                 <div class="col-md-3 m-0 p-0">
+                    <a href="<?= site_url() . '/bio-details';?>">
                     <div class="person-image person-image-1" style="background-image: url(<?php echo site_url() . $image_dir ?>person1.png);">
                         <p>
                         Avery <br>Cannon
                         </p>
                         <div class="divider-person"></div>
                     </div>
+                    </a>
                 </div>
                 <div class="col-md-3 m-0 p-0">
+                    <a href="<?= site_url() . '/bio-details';?>">
                     <div class="person-image person-image-1" style="background-image: url(<?php echo site_url() . $image_dir ?>person1.png);">
                         <p>
                         Avery <br>Cannon
                         </p>
                         <div class="divider-person"></div>
                     </div>
+                    </a>
                 </div>
                 <div class="col-md-3 m-0 p-0">
+                    <a href="<?= site_url() . '/bio-details';?>">
                     <div class="person-image person-image-1" style="background-image: url(<?php echo site_url() . $image_dir ?>person1.png);">
                         <p>
                         Avery <br>Cannon
                         </p>
                         <div class="divider-person"></div>
                     </div>
-                </div>
-                <div class="col-md-3 m-0 p-0">
-                    <div class="person-image person-image-1" style="background-image: url(<?php echo site_url() . $image_dir ?>person1.png);">
-                        <p>
-                        Avery <br>Cannon
-                        </p>
-                        <div class="divider-person"></div>
-                    </div>
+                    </a>
                 </div>
             </div>
 
@@ -221,37 +262,44 @@ get_header();
                         </p>
 
                     </div>
+                    
                 </div>
                 <div class="col-md-3 m-0 p-0">
+                    <a href="<?= site_url() . '/bio-details';?>">
                     <div class="person-image person-image-1" style="background-image: url(<?php echo site_url() . $image_dir ?>tod.png);">
                         <p>
                         Tod <br>Francis
                         </p>
                         <div class="divider-person"></div>
                     </div>
+                    </a>
                 </div>
                 <div class="col-md-3 m-0 p-0">
+                    <a href="<?= site_url() . '/bio-details';?>">
                     <div class="person-image person-image-1" style="background-image: url(<?php echo site_url() . $image_dir ?>person1.png);">
                         <p>
                         Rob <br>Coneybeer
                         </p>
                         <div class="divider-person"></div>
                     </div>
+                    </a>
                 </div>
                 <div class="col-md-3 m-0 p-0">
+                    <a href="<?= site_url() . '/bio-details';?>">
                     <div class="person-image person-image-1" style="background-image: url(<?php echo site_url() . $image_dir ?>austin.png);">
                         <p>
                         Austin <br>Grose
                         </p>
                         <div class="divider-person"></div>
                     </div>
+                    </a>
                 </div>
             </div>
         </div>
     </div>
 
     <div class="footer-links">
-        <div class="container">
+        <div class="container-fluid">
             <div class="row">
                 <div class="col-md-3">
                     <div class="footer footer-1">
