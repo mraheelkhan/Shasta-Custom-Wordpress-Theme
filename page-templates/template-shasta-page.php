404569cc4cb79d72fad16fbf21b7ff73effc3649
Raheel Khan <irahilkhan@gmail.com>
2020-09-23 00:01:12 +0500

    Shasta, bio, bio-details ACF

--- page-templates/template-shasta-page.php
+++ page-templates/template-shasta-page.php
@@ -42,14 +42,14 @@ get_header();
             'post_type'			=> 'bio'
         ));
     ?>
-    <pre>
-	<?= print_r($bios); ?>
-	</pre>
+   
     <div class="shasta-persons-list">
         <div class="container-fluid">
             <div class="row">
                <?php if ($bios): ?>
                 <?php foreach( $bios as $bio ): ?>
+				<?php $isFounder = get_field('is_founder', $bio->ID); ?>
+				<?php if ($isFounder != 1): ?>
                     <?php setup_postdata( $bio ); ?>
                         <div class="col-sm-6 col-md-6 col-lg-3 m-0 p-0">
                             <a href="<?= $bio->guid; ?>">
@@ -67,7 +67,9 @@ get_header();
                             </a>
                         </div>
                     <?php wp_reset_postdata(); ?>
+				<?php endif ?>
                 <?php endforeach; ?>
+				 
                 <?php endif ?>
                 
                 
@@ -85,36 +87,32 @@ get_header();
                     </div>
                     
                 </div>
-                <div class="col-sm-6 col-md-6 col-lg-3 m-0 p-0">
-                    <a href="<?= site_url() . '/bio-details';?>">
-                    <div class="person-image person-image-1" style="background-image: url(<?php echo site_url() . $image_dir ?>tod.png);">
-                        <p>
-                        Tod <br>Francis
-                        </p>
-                        <div class="divider-person"></div>
-                    </div>
-                    </a>
-                </div>
-                <div class="col-sm-6 col-md-6 col-lg-3 m-0 p-0">
-                    <a href="<?= site_url() . '/bio-details';?>">
-                    <div class="person-image person-image-1" style="background-image: url(<?php echo site_url() . $image_dir ?>person1.png);">
-                        <p>
-                        Rob <br>Coneybeer
-                        </p>
-                        <div class="divider-person"></div>
-                    </div>
-                    </a>
-                </div>
-                <div class="col-sm-6 col-md-6 col-lg-3 m-0 p-0">
-                    <a href="<?= site_url() . '/bio-details';?>">
-                    <div class="person-image person-image-1" style="background-image: url(<?php echo site_url() . $image_dir ?>austin.png);">
-                        <p>
-                        Austin <br>Grose
-                        </p>
-                        <div class="divider-person"></div>
-                    </div>
-                    </a>
-                </div>
+                <?php if ($bios): ?>
+				
+                <?php foreach( $bios as $bio ): ?>
+				<?php $isFounder = get_field('is_founder', $bio->ID); ?>
+				<?php if ($isFounder == 1): ?>
+                    <?php setup_postdata( $bio ); ?>
+                        <div class="col-sm-6 col-md-6 col-lg-3 m-0 p-0">
+                            <a href="<?= $bio->guid; ?>">
+                            <img class="image_overlay" style="max-width: 100%; width:100%" src="<?php the_field('image', $bio->ID); ?>"/>
+                            <div class="overlay">
+                                <h2 class="overlay-text-content"><?php the_field("designations", $bio->ID); ?></h2>
+                            </div>
+                            <div class="person-info-detail">
+                                <p>
+                                <?php the_field("first_name", $bio->ID); ?> <br><?php the_field("last_name", $bio->ID); ?>
+                                </p>
+                                <div class="divider-person"></div>
+                            </div>
+                            
+                            </a>
+                        </div>
+                    <?php wp_reset_postdata(); ?>
+				 <?php endif ?>
+                <?php endforeach; ?>
+				
+                <?php endif ?>
             </div>
         </div>
     </div>
@@ -124,37 +122,57 @@ get_header();
             <div class="row">
                 <div class="col-md-3">
                     <div class="footer footer-1">
-                        <h3>How To</h3>
+                        <h3><?php the_field('quick_link_header_text_1'); ?></h3>
                         <div class="divider-footer"></div>
                         <ul class="footer-links-list">
-                            <li>TechCrunch: As Stocks recover, private investors aren’t buying the hype</li>
+                            <li>
+								<?php $link = get_field('quick_link_hyperlink_1'); $text = $link['title']; $url = $link['url']; ?>
+								<a href="<?php echo $url ?>" style="color: #00a7e1">
+									<?php echo $text ?>
+								</a>
+							</li>
                         </ul>
                     </div>
                 </div>
                 <div class="col-md-3">
                     <div class="footer footer-1">
-                        <h3>How To</h3>
+                        <h3><?php the_field('quick_link_header_text_2'); ?></h3>
                         <div class="divider-footer"></div>
                         <ul class="footer-links-list">
-                            <li>TechCrunch: As Stocks recover, private investors aren’t buying the hype</li>
+                            <li>
+								<?php $link = get_field('quick_link_hyperlink_2'); $text = $link['title']; $url = $link['url']; ?>
+								<a href="<?php echo $url ?>" style="color: #00a7e1">
+									<?php echo $text ?>
+								</a>
+							</li>
                         </ul>
                     </div>
                 </div>
                 <div class="col-md-3">
                     <div class="footer footer-1">
-                        <h3>How To</h3>
+                        <h3><?php the_field('quick_link_header_text_3'); ?></h3>
                         <div class="divider-footer"></div>
                         <ul class="footer-links-list">
-                            <li>TechCrunch: As Stocks recover, private investors aren’t buying the hype</li>
+                            <li>
+								<?php $link = get_field('quick_link_hyperlink_3'); $text = $link['title']; $url = $link['url']; ?>
+								<a href="<?php echo $url ?>" style="color: #00a7e1">
+									<?php echo $text ?>
+								</a>
+							</li>
                         </ul>
                     </div>
                 </div>
                 <div class="col-md-3">
                     <div class="footer footer-1">
-                        <h3>How To</h3>
+                        <h3><?php the_field('quick_link_header_text_4'); ?></h3>
                         <div class="divider-footer"></div>
                         <ul class="footer-links-list">
-                            <li>TechCrunch: As Stocks recover, private investors aren’t buying the hype</li>
+                           <li>
+								<?php $link = get_field('quick_link_hyperlink_4'); $text = $link['title']; $url = $link['url']; ?>
+								<a href="<?php echo $url ?>" style="color: #00a7e1">
+									<?php echo $text ?>
+								</a>
+							</li>
                         </ul>
                     </div>
                 </div>
